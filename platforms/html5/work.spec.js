4d93a3836fa5cd895daa23a040dfabb1c0db6f50
James Garner <j@jada.io>
2018-10-15 01:28:36 +0200

    pass 2 of html5 snapshot

--- platforms/html5/work.spec.js
+++ platforms/html5/work.spec.js
@@ -4,13 +4,22 @@ const classFactory = require('./classes/factory.js')
 test('works', () => {
   const team = classFactory.team()
   const project = classFactory.project()
-  const resources = [
+  let resources = [
     classFactory.resource({
       type: 'space',
       name: 'Bird Space',
       events: {}
     })
   ]
+  // jest hacks
+  team.id = 'jest-hack-team-id'
+  project.id = 'jest-hack-project-id'
+  resources = resources.map(resource => {
+    resource.id = `jest-hack-${resource.name}`
+    resource.createdAt = 0
+    return resource
+  })
+  //
 
   return work(team, project, resources)
     .then(result => {
