00bdf52a634789c9dc1eb39a73da537acadb4ca5
James Garner <j@jada.io>
2018-10-28 22:49:56 +0000

    dropper component

---
+++ app/src/components/Dropper/Dropper.js
@@ -0,0 +1,87 @@
+import React, { PureComponent, Fragment } from 'react'
+import PropTypes from 'prop-types'
+import styled from 'styled-components'
+import uuid from 'uuid'
+
+import { font, colours } from '../../styleAbstractions'
+
+const StyledDropper = styled.div`
+  label {
+    display: block;
+    width: 100%;
+    ${font}
+    font-size: 80%;
+    color: #bdc3c7;
+  }
+  select {
+    display: block;
+    margin-top: 0.5rem;
+    transition: border-color 0.2s ease-in-out, opacity 0.2s ease-in-out;
+    ${font}
+    background-color: #dadfe1;
+    color: ${colours.fore};
+    border-radius: 4px;
+    border-width: 2px;
+    border-style: solid;
+    border-color: #dadfe1;
+    outline: 0;
+    &[disabled] {
+      opacity: 0.5;
+    }
+    :focus {
+      border-color: ${colours.outline};
+    }
+  }
+`
+
+class Dropper extends PureComponent {
+  constructor(props) {
+    super(props)
+    this.state = {
+      id: uuid(),
+      required: (typeof props.required === 'boolean' ? props.required : false),
+      disabled: props.disabled || false,
+      value: props.value
+    }
+    this.handleOnChange = this.handleOnChange.bind(this)
+  }
+
+  handleOnChange(e) {
+    const value = e.target.value
+    this.setState({
+      value
+    })
+    this.props.onChoose(value)
+  }
+
+  render() {
+    return (
+      <StyledDropper className='component--dropper'>
+        {this.props.label && <label htmlFor={`component--dropper-${this.state.id}`}>{this.props.label}</label>}
+        <select id={`component--dropper-${this.state.id}`} value={this.state.value} onChange={this.handleOnChange} required={this.props.required}>
+          {this.props.options.map(option => {
+            return <option value={option.id} key={option.id}>{option.name}</option>
+          })}
+        </select>
+      </StyledDropper>
+    )
+  }
+}
+
+Dropper.propTypes = {
+  required: PropTypes.bool,
+  disabled: PropTypes.bool,
+  label: PropTypes.string,
+  options: PropTypes.array.isRequired,
+  value: PropTypes.string,
+  onChoose: PropTypes.func
+}
+
+Dropper.defaultProps = {
+  required: false,
+  disabled: false,
+  value: 'none',
+  onChoose: () => {}
+}
+
+export default Dropper
---
+++ app/src/components/Dropper/Dropper.spec.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import renderer from 'react-test-renderer'
+import 'jest-styled-components'
+import Dropper from './Dropper'
+
+it('renders', () => {
+  const options = [
+    {
+      id: 'option-1',
+      name: 'Option 1'
+    },
+    {
+      id: 'option-2',
+      name: 'Option 2'
+    },
+    {
+      id: 'option-3',
+      name: 'Option 3'
+    }
+  ]
+  const tree = renderer
+    .create(
+      <Dropper options={options} />
+    )
+    .toJSON()
+  expect(tree).toMatchSnapshot()
+})
