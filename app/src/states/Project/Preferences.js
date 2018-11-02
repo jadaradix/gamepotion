93e2b2c840924983b3d4c4b6ede70b630049de29
James Garner <j@jada.io>
2018-11-02 20:39:26 +0000

    project preferences screen copied from auth; looks better

--- app/src/states/Project/Preferences.js
+++ app/src/states/Project/Preferences.js
@@ -1,21 +1,39 @@
 import React, { PureComponent, Fragment } from 'react'
+import { Redirect } from 'react-router'
+import styled from 'styled-components'
 
+import Box from '../../components/Box/Box'
 import Heading1 from '../../components/Heading1/Heading1'
 
 import MainToolbarContainer from '../../component-instances/MainToolbarContainer'
 import ResponsiveContainer from '../../component-instances/ResponsiveContainer'
 
-class StateProjectPreferences extends PureComponent {
+const StyledState = styled.div`
+  .component--box {
+    max-width: 640px;
+    margin: 4rem auto 0 auto;
+  }
+`
+
+class StateAuth extends PureComponent {
+  constructor(props) {
+    super(props)
+  }
+
   render() {
     return (
       <Fragment>
         <MainToolbarContainer />
         <ResponsiveContainer>
-          <Heading1>Project preferences</Heading1>
+          <StyledState>
+            <Box>
+              <Heading1>Project preferences</Heading1>
+            </Box>
+          </StyledState>
         </ResponsiveContainer>
       </Fragment>
     )
   }
 }
 
-export default StateProjectPreferences
+export default StateAuth
