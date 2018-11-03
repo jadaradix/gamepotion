598d21e0bb6cac5aa7899b6ee681e277e54567e3
James Garner <j@jada.io>
2018-11-03 02:05:00 +0000

    minor work on canvasRenderSpace, which is going to go away soon

--- abstractions/canvasRenderSpace/index.js
+++ abstractions/canvasRenderSpace/index.js
@@ -1,4 +1,12 @@
-const canvasRenderSpace = (canvasElement, resources, space) => {
+const canvasRenderSpace = (element, space, resources) => {
+  ((e) => {
+    e.width = space.width
+    e.height = space.height
+    e.style.display = 'block'
+    e.style.backgroundColor = 'black'
+    e.style.width = space.width
+    e.style.height = space.height
+  })(element)
   return null
 }
 
--- abstractions/canvasRenderSpace/index.spec.js
+++ abstractions/canvasRenderSpace/index.spec.js
@@ -2,7 +2,10 @@ import canvasRenderSpace from './index.js'
 
 describe('canvasRenderSpace', () => {
   it('works', () => {
-    const c = canvasRenderSpace()
+    const element = null
+    const space = null
+    const resources = []
+    const c = canvasRenderSpace(element, space, resources)
     expect(c).toBe(null)
   })
 })
