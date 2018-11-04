d6c2691647cdab97880a6290b4114dc6ec7c9c21
James Garner <j@jada.io>
2018-11-04 18:46:53 +0000

    state for uploader done (missed it)

--- app/src/components/Uploader/Uploader.js
+++ app/src/components/Uploader/Uploader.js
@@ -14,13 +14,22 @@ const StyledUploader = styled.div`
   border: 2px dashed #dadfe1;
   border-radius: 4px;
   cursor: copy;
+  &.component--uploader-in-progress {
+    cursor: wait;
+  }
+  &.component--uploader-done {
+    cursor: default;
+  }
   img {
     display: block;
     width: 48px;
+    height: 48px;
     margin: 1rem auto 0.25rem auto;
     // background-color: yellow;
   }
   img.icon--upload {
+  }
+  img.icon--upload-done {
   }
   img.icon--loading {
     animation: gmc-loading 4s linear infinite;
@@ -38,44 +47,73 @@ class Uploader extends PureComponent {
   constructor(props) {
     super(props)
     this.state = {
-      inProgress: false
+      inProgress: false,
+      isDone: false
     }
     this.onDropAccepted = this.onDropAccepted.bind(this)
     this.onFileDialogCancel = this.onFileDialogCancel.bind(this)
+    this.doneTimeout = null
+  }
+
+  componentWillUnmount() {
+    window.clearTimeout(this.doneTimeout)
   }
 
   onDropAccepted(files) {
     if (files.length === 0) return
     this.setState({
-      inProgress: true
+      inProgress: true,
+      isDone: false
     })
     const formData = new FormData()
     formData.append('bin', files[0])
     api.uploadBin(this.props.route, formData)
       .then(() => {
         this.setState({
-          inProgress: false
+          inProgress: false,
+          isDone: true
         })
+        this.doneTimeout = window.setTimeout(() => {
+          this.setState({
+            inProgress: false,
+            isDone: false
+          })
+        }, 1000)
         this.props.onDone()
       })
   }
 
   onFileDialogCancel() {
     this.setState({
-      inProgress: false
+      inProgress: false,
+      isDone: false
     })
   }
 
   render() {
+    const className = (() => {
+      if (this.state.inProgress) {
+        return 'component--uploader-in-progress'
+      }
+      if (this.state.isDone) {
+        return 'component--uploader-done'
+      }
+    })()
     return (
-      <StyledUploader className='component--uploader'>
+      <StyledUploader className={`component--uploader ${className}`}>
+        {this.state.isDone === true &&
+          <Fragment>
+            <img src={icons.generic.uploadDone} className='icon--upload-done' alt='' />
+            <p>Done!</p>
+          </Fragment>
+        }
         {this.state.inProgress === true &&
           <Fragment>
-            <img src={icons.generic.loading} className='icon--loading' alt='' />
+            <img src={icons.generic.uploadInProgress} className='icon--loading' alt='' />
             <p>Uploading...</p>
           </Fragment>
         }
-        {this.state.inProgress === false &&
+        {this.state.inProgress === false && this.state.isDone === false &&
           <Dropzone
             accept={this.props.mimeTypes.join(', ')}
             onDropAccepted={this.onDropAccepted}
