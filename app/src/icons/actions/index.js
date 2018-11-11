8841a3c90a70beaca373a5180d92d1a65cb3182d
James Garner <j@jada.io>
2018-11-11 20:52:16 +0000

    reorder actions

--- app/src/icons/actions/index.js
+++ app/src/icons/actions/index.js
@@ -1,19 +1,19 @@
-import Debug from './files/Debug.svg'
-import SetXSpeed from './files/SetXSpeed.svg'
-import SetYSpeed from './files/SetYSpeed.svg'
 import Create from './files/Create.svg'
 import Destroy from './files/Destroy.svg'
-import SetVariable from './files/SetVariable.svg'
+import SetXSpeed from './files/SetXSpeed.svg'
+import SetYSpeed from './files/SetYSpeed.svg'
 import If from './files/If.svg'
 import EndIf from './files/EndIf.svg'
+import SetVariable from './files/SetVariable.svg'
+import Debug from './files/Debug.svg'
 
 export default {
-  Debug,
-  SetXSpeed,
-  SetYSpeed,
   Create,
   Destroy,
-  SetVariable,
+  SetXSpeed,
+  SetYSpeed,
   If,
-  EndIf
+  EndIf,
+  SetVariable,
+  Debug
 }
--- classes/classes/actions/index.js
+++ classes/classes/actions/index.js
@@ -1,19 +1,19 @@
-import Debug from './Debug.js'
-import SetXSpeed from './SetXSpeed.js'
-import SetYSpeed from './SetYSpeed.js'
 import Create from './Create.js'
 import Destroy from './Destroy.js'
-import SetVariable from './SetVariable.js'
+import SetXSpeed from './SetXSpeed.js'
+import SetYSpeed from './SetYSpeed.js'
 import If from './If.js'
 import EndIf from './EndIf.js'
+import SetVariable from './SetVariable.js'
+import Debug from './Debug.js'
 
 export default {
-  Debug,
-  SetXSpeed,
-  SetYSpeed,
   Create,
   Destroy,
-  SetVariable,
+  SetXSpeed,
+  SetYSpeed,
   If,
-  EndIf
+  EndIf,
+  SetVariable,
+  Debug
 }
