38ca7d8631581ee392a9b7d5a4a7c701ab819798
James Garner <j@jada.io>
2018-12-03 11:13:35 +0100

    bump gatsby cli deps, delete stuff we dont need

--- site/src/src/pages/index.js
+++ site/src/src/pages/index.js
@@ -6,9 +6,8 @@ import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hi!</h1>
+    <p>Welcome to the website.</p>
     <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
       <Image />
     </div>
