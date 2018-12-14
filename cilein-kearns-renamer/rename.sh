bd79b1602b5f1a044e14e78e2f0c6333ae78a34e
James Garner <j@jada.io>
2018-12-14 23:18:36 +0000

    the kearns renamer

---
+++ cilein-kearns-renamer/rename.sh
@@ -0,0 +1,5 @@
+node rename.js "Aquatic Pack" >> rename2.sh;
+node rename.js "Asteroids X" >> rename2.sh;
+node rename.js "PacMan" >> rename2.sh;
+node rename.js "Shooter Pack" >> rename2.sh;
+node rename.js "Space Scroller Pack" >> rename2.sh;
