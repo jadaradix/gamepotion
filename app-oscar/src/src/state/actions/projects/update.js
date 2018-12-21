0723bb772d6ff4f061548624a2d5b9b2f02ab9f3
James Garner <j@jada.io>
2018-12-21 00:02:52 +0000

    undo 'try to fix' of projects update

--- app-oscar/src/src/state/actions/projects/update.js
+++ app-oscar/src/src/state/actions/projects/update.js
@@ -1,6 +1,5 @@
 import debounce from 'debounce'
 import api from '../../../api'
-import getProjects from './get'
 
 const patch = debounce((projectId, payload) => {
   api.patch('api-core', `me/team/projects/${projectId}`, payload)
@@ -8,25 +7,10 @@ const patch = debounce((projectId, payload) => {
 
 export default async function (state, payload) {
   patch(payload.id, payload)
-  let { projects } = await getProjects(state)
-  if (state.currentProject !== null && state.currentProject.project.id === payload.id) {
-    state.currentProject.project.fromApiPatch(payload)
-  }
-  projects = projects.map(p => {
-    if (p.project.id === payload.id) {
-      p.project.fromApiPatch(payload)
-    }
-    return p
-  })
-  const currentProject = (() => {
-    if (state.currentProject === null) {
-      return null
-    }
-    return projects.find(p => p.project.id === state.currentProject.project.id) || null
-  })()
-  return Promise.resolve({
+  const currentProject = state.currentProject
+  currentProject.project.fromApiPatch(payload)
+  return {
     ...state,
-    projects,
     currentProject
-  })
+  }
 }
