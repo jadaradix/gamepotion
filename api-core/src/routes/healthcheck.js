7c1a0893cd23ba51e9d2149100cabd8d65bfb3ab
James Garner <j@jada.io>
2019-01-07 23:09:40 +0000

    assert in sendMail, more logging, try catch in healthcheck route

--- api-core/src/routes/healthcheck.js
+++ api-core/src/routes/healthcheck.js
@@ -1,11 +1,20 @@
 const sendMail = require('../sendMail.js')
 
 const route = async (request, response, next) => {
-  await sendMail('Sample Message')
-  response.header('content-type', 'text/plain')
-  response.send('ok!')
-  return next()
+  try {
+    await sendMail({
+      subject: 'Sample Message',
+      to: 'me@newty.org',
+      contentText: 'hey there',
+      contentHtml: '<p>hey there!</p>'
+    })
+    response.header('content-type', 'text/plain')
+    response.send('ok!')
+    return next()
+  } catch (error) {
+    response.send(500, 'could not perform healthcheck')
+    return next()
+  }
 }
 
 module.exports = route
-
--- api-core/src/sendMail.js
+++ api-core/src/sendMail.js
@@ -1,21 +1,31 @@
+const assert = require('assert').strict
 const got = require('got')
 
 const SERVICE_MAIL_URL = 'http://oscar-production-service-mail:1031'
 
-const sendMail = async (subject) => {
+const sendMail = async ({
+  subject,
+  to,
+  contentText,
+  contentHtml = contentText
+}) => {
+  assert(typeof subject === 'string', 'you need to provide a subject')
+  assert(typeof to === 'string', 'you need to provide a to')
+  assert(typeof contentText === 'string', 'you need to provide a contentText')
+  assert(typeof contentHtml === 'string', 'you need to provide a contentHtml')
   try {
     await got(`${SERVICE_MAIL_URL}/v1/mail`, {
       method: 'post',
       json: true,
       body: {
-        to: 'me@newty.org',
+        to,
         subject,
-        contentText: 'Some Content',
-        contentHtml: '<p>Some HTML </p>'
+        contentText,
+        contentHtml
       }
     })
   } catch (error) {
-    console.error(error)
+    console.error('[sendMail] error', error)
     throw new Error('Could not send message')
   }
 }
