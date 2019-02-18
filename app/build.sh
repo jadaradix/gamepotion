f8db0ef5a337bd3f82877841c48c4ddfd8c4c804
James Garner <j@jada.io>
2019-02-18 12:55:26 +0000

    step 3

--- app/build.sh
+++ app/build.sh
@@ -3,9 +3,9 @@ cd src;
   npm run build;
 cd ..;
 
-docker stop gamepotion-app-oscar;
-docker rm gamepotion-app-oscar;
-docker build -t gamepotion-app-oscar .;
+docker stop gamepotion-app;
+docker rm gamepotion-app;
+docker build -t gamepotion-app .;
 
-docker tag gamepotion-app-oscar eu.gcr.io/thegmc-219013/gamepotion-app-oscar:latest;
-docker push eu.gcr.io/thegmc-219013/gamepotion-app-oscar:latest;
+docker tag gamepotion-app eu.gcr.io/thegmc-219013/gamepotion-app:latest;
+docker push eu.gcr.io/thegmc-219013/gamepotion-app:latest;
